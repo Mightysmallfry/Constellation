bba504d29d81bd25e60959847f492196daa9ecf4
Mighty <Jonathan.Marien@live.com>
2026-09-18 17:43:50 -0700

    Added library wrapping header

---
+++ include/Constellation.h
@@ -0,0 +1,11 @@
+#ifndef CONSTELLATION_LIBRARY_MANIFEST
+#define CONSTELLATION_LIBRARY_MANIFEST
+
+#include "Logger.h"
+#include "Containers/Handle.h"
+#include "Containers/SparseSet.h"
+#include "Math/MathUtils.h"
+#include "Math/Vector2.h"
+#include "Math/Vector3.h"
+
+#endif
